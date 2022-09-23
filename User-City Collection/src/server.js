4621b9bacd68c7447cea784cd33c869a49c4c414
Krunal Borhade <krunalborhade2897@gmail.com>
2022-09-23 09:32:51 +0530

    Done

---
+++ User-City Collection/src/server.js
@@ -0,0 +1,15 @@
+const connect = require("./config/db")
+require("dotenv").config()
+port = process.env.PORT
+
+const app = require("./index")
+
+app.listen(port, async ()=>{
+    
+    try {
+        await connect()
+    } catch (err) {
+        return res.status(500).send({message:err.message})
+    }
+    console.log("Listening on port 5000")
+})
